452f9da372c2302798d7c2c19d2ea1ca2768a6f8
allixender@googlemail.com <allixender@googlemail.com>
2014-04-12 23:17:41 +1200

    gui reads pic from mqtt and upates panel
    but I don't get it in loop mode yet

--- simplegui/src/main/java/spaceapps/challenge/simplegui/App.java
+++ simplegui/src/main/java/spaceapps/challenge/simplegui/App.java
@@ -2,94 +2,195 @@ package spaceapps.challenge.simplegui;
 
 //Imports are listed in full to show what's being used
 //could just import javax.swing.* and java.awt.* etc..
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.JComboBox;
 import javax.swing.JButton;
 import javax.swing.JLabel;
-import javax.swing.JList;
+import javax.swing.SwingUtilities;
+import javax.swing.SwingWorker;
+
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import org.fusesource.mqtt.client.BlockingConnection;
+import org.fusesource.mqtt.client.MQTT;
+import org.fusesource.mqtt.client.Message;
+import org.fusesource.mqtt.client.QoS;
+import org.fusesource.mqtt.client.Topic;
 
 /**
  * Hello world!
  * 
  */
-public class App {
-	public static void main(String[] args) {
-		System.out.println("NASA Space Apps Challenge");
+public class App extends JFrame {
+	
+	public void update(Graphics g) {
 
-		new App();
+		Graphics2D g2d = (Graphics2D) g;
+
+		System.out.println("try update");
+
+		try {
+
+			String mqttHost = "tcp://172.16.55.197:1883";
+			String mqttClientId = "javahud";
+			final String mqttTopic = "tetris/pics/raw";
+
+			MQTT mqtt = new MQTT();
+			mqtt.setHost(mqttHost);
+			mqtt.setClientId(mqttClientId);
+
+			BlockingConnection connection = mqtt.blockingConnection();
+			connection.connect();
+			System.out.println("connected to channels");
+			Topic[] topics = { new Topic(mqttTopic, QoS.AT_LEAST_ONCE) };
+			byte[] qoses = connection.subscribe(topics);
+
+				Message message = connection.receive();
+				System.out.println(message.getTopic());
+				byte[] payload = message.getPayload();
+				System.out.println(payload.length);
+				ByteArrayInputStream bis = new ByteArrayInputStream(payload);
+				javax.imageio.stream.ImageInputStream iis = ImageIO
+						.createImageInputStream(bis);
+				BufferedImage mqPic = ImageIO.read(iis);
+
+				// process the message then:
+
+				message.ack();
+				
+
+				getContentPane().removeAll();
+				final JPanel comboPanel = new JPanel();
+				getContentPane().add(comboPanel);
+				JLabel comboLbl = new JLabel("HUD:");
+
+				comboPanel.add(comboLbl);
+
+				JButton exitButton = new JButton("Exit");
+
+				// The ActionListener class is used to handle the
+				// event that happens when the user clicks the button.
+				// As there is not a lot that needs to happen we can
+				// define an anonymous inner class to make the code simpler.
+				exitButton.addActionListener(new ActionListener() {
+					@Override
+					public void actionPerformed(ActionEvent event) {
+						// When the fruit of veg button is pressed
+						System.exit(0);
+
+					}
+				});
+
+				// The JFrame uses the BorderLayout layout manager.
+				// Put the two JPanels and JButton in different areas.
+				add(comboPanel, BorderLayout.NORTH);
+				add(exitButton, BorderLayout.SOUTH);
+
+				// make sure the JFrame is visible
+
+				// make sure the program exits when the frame closes
+				setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				setTitle("Example HUD GUI");
+				setSize(800, 600);
+				JLabel mqLabel = new JLabel(new ImageIcon(mqPic));
+				comboPanel.add(mqLabel);
+				repaint();
+				validate();
+			
+			
+			
+
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
 
-	public App() {
+	public App() throws IOException {
 
-		JFrame guiFrame = new JFrame();
-
-		// make sure the program exits when the frame closes
-		guiFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		guiFrame.setTitle("Example GUI");
-		guiFrame.setSize(300, 250);
-
-		// This will center the JFrame in the middle of the screen
-		guiFrame.setLocationRelativeTo(null);
-
-		// Options for the JComboBox
-		String[] fruitOptions = { "Apple", "Apricot", "Banana", "Cherry",
-				"Date", "Kiwi", "Orange", "Pear", "Strawberry" };
-
-		// Options for the JList
-		String[] vegOptions = { "Asparagus", "Beans", "Broccoli", "Cabbage",
-				"Carrot", "Celery", "Cucumber", "Leek", "Mushroom", "Pepper",
-				"Radish", "Shallot", "Spinach", "Swede", "Turnip" };
-
-		// The first JPanel contains a JLabel and JCombobox
 		final JPanel comboPanel = new JPanel();
-		JLabel comboLbl = new JLabel("Fruits:");
-		JComboBox fruits = new JComboBox(fruitOptions);
+		getContentPane().add(comboPanel);
+		JLabel comboLbl = new JLabel("HUD:");
 
-		
 		comboPanel.add(comboLbl);
-		comboPanel.add(fruits);
 
-		// Create the second JPanel. Add a JLabel and JList and
-		// make use the JPanel is not visible.
-		final JPanel listPanel = new JPanel();
-		listPanel.setVisible(false);
-		JLabel listLbl = new JLabel("Vegetables:");
-		JList vegs = new JList(vegOptions);
-		vegs.setLayoutOrientation(JList.HORIZONTAL_WRAP);
-
-		listPanel.add(listLbl);
-		listPanel.add(vegs);
-
-		JButton vegFruitBut = new JButton("Fruit or Veg");
+		JButton exitButton = new JButton("Exit");
 
 		// The ActionListener class is used to handle the
 		// event that happens when the user clicks the button.
 		// As there is not a lot that needs to happen we can
 		// define an anonymous inner class to make the code simpler.
-		vegFruitBut.addActionListener(new ActionListener() {
+		exitButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent event) {
 				// When the fruit of veg button is pressed
-				// the setVisible value of the listPanel and
-				// comboPanel is switched from true to
-				// value or vice versa.
-				listPanel.setVisible(!listPanel.isVisible());
-				comboPanel.setVisible(!comboPanel.isVisible());
+				System.exit(0);
+
+			}
+		});
+
+		JButton smtButton = new JButton("update");
+		smtButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent event) {
+				// When the fruit of veg button is pressed
+				update(getGraphics());
 
 			}
 		});
 
 		// The JFrame uses the BorderLayout layout manager.
 		// Put the two JPanels and JButton in different areas.
-		guiFrame.add(comboPanel, BorderLayout.NORTH);
-		guiFrame.add(listPanel, BorderLayout.CENTER);
-		guiFrame.add(vegFruitBut, BorderLayout.SOUTH);
+		add(comboPanel, BorderLayout.NORTH);
+		add(smtButton, BorderLayout.SOUTH);
+		add(exitButton);
+
+		
 
 		// make sure the JFrame is visible
-		guiFrame.setVisible(true);
+
+		// make sure the program exits when the frame closes
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setTitle("Example HUD GUI");
+		setSize(800, 600);
+		BufferedImage myPicture = ImageIO.read(new File(
+				"C:\\Users\\akmoch\\Desktop\\default.jpg"));
+		JLabel mqLabel = new JLabel(new ImageIcon(myPicture));
+		comboPanel.add(mqLabel);
+	}
+
+	public static void main(String[] args) {
+		
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				System.out.println("NASA Space Apps Challenge");
+
+				try {
+					App theApp = new App();
+					theApp.setVisible(true);
+					
+
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+			}
+		});
+
 	}
 }
