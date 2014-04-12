01d35e05ca8943ed81d6c9facbdffe16f2eb2209
allixender@googlemail.com <allixender@googlemail.com>
2014-04-12 15:48:06 +1200

    added maven simple gui skeleton for the HUD demo

---
+++ simplegui/src/main/java/spaceapps/challenge/simplegui/App.java
@@ -0,0 +1,13 @@
+package spaceapps.challenge.simplegui;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
